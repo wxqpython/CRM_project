6ee6b28c01518cc3a383da5871f19d54614f8593
wxqpython <1312325639@qq.com>
2018-01-09 19:59:28 +0800

    Update 自定义admin.py

--- 自定义admin.py
+++ 自定义admin.py
@@ -1,6 +1,20 @@
+# 自定义Django admin 
+
 from django.template import admin
 from app01 import models
-
-
 admin.site.registry(models.UserInfo)
+
+# include
+partterns = [
+   url(r'^admin', admin.site.urls),            # admin.site.urls 返回形如（[],None,None）的元组.
+   url(r'^backend', include('backend.urls')),  # include 同样返回形如（[],None,None）的元组.
+   # 也可以写成形如 include([],None,None) 格式.
+ ]
+
+
+
+
+
+
+
 
