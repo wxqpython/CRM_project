533e90d9879379c3f84d95130db8dfb4be95c2e7
wxqpython <1312325639@qq.com>
2018-01-23 22:12:01 +0800

    Update 自定义admin.py

--- 自定义admin.py
+++ 自定义admin.py
@@ -1,4 +1,18 @@
 
+
+request.GET 数据类型为QueryDict 本身不可修改，必须修改属性才能被修改_mutable = True
+      
+request.GET._mutable = True
+
+# 运用
+import copy
+params = request.GET()
+params._mutable = True
+params["page"] = 2      # QueryDict()
+params.urlencode()      # 转为了page=1&key=xx
+
+
+
       
       
       
